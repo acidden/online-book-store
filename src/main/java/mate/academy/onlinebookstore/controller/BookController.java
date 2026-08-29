b4c5706362feb4d3a0413aeccf8ebea39096fb82
Den <99994444@ukr.net>
2026-08-29 09:59:09 +0300

    added globalException and validation for dto

--- src/main/java/mate/academy/onlinebookstore/controller/BookController.java
+++ src/main/java/mate/academy/onlinebookstore/controller/BookController.java
@@ -1,5 +1,6 @@
 package mate.academy.onlinebookstore.controller;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.onlinebookstore.dto.BookDto;
@@ -35,7 +36,7 @@ public class BookController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
+    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto bookDto) {
         return bookService.createBook(bookDto);
     }
 
--- src/main/java/mate/academy/onlinebookstore/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/onlinebookstore/dto/CreateBookRequestDto.java
@@ -1,13 +1,21 @@
 package mate.academy.onlinebookstore.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank(message = "Title cannot be empty")
     private String title;
+    @NotBlank(message = "Author cannot be empty")
     private String author;
+    @NotBlank(message = "Isbn cannot be empty")
     private String isbn;
+    @NotNull(message = "Price cannot be empty")
+    @PositiveOrZero(message = "Price must be >= 0")
     private BigDecimal price;
     private String description;
     private String coverImage;
